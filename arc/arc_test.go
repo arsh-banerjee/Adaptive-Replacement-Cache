5239feb702aa2310cca47390ceb7f6bdd7363931
arsh-banerjee <arshb@princeton.edu>
2022-12-15 16:33:19 -0500

    Adding tests and test helper functions

--- arc/arc_test.go
+++ arc/arc_test.go
@@ -8,10 +8,23 @@ Description:
 package arc
 
 import (
+	"log"
+	"strconv"
 	"testing"
 )
 
 // Helper Functions:
+// Checks to see if Get exists and returns correct value
+func confirmGet(arc *ARC, val string) {
+	Val, err := arc.Get(val)
+	if err != true {
+		log.Fatalf("Key: %s not found", val)
+	}
+	Value := string(Val)
+	if Value != val {
+		log.Fatalf("Get of key: %s is %s, should be %s", val, Value, val)
+	}
+}
 
 func TestArcInit(t *testing.T) {
 	arc := NewArc(8)
@@ -29,5 +42,19 @@ func TestArcInit(t *testing.T) {
 func TestTooSmallArcInit(t *testing.T) {
 	if NewArc(0) != nil || NewArc(1) != nil {
 		t.Errorf("initializing an ARC object even when size is incorrect")
+	}
+}
+
+func TestAddingUntilCapacity(t *testing.T) {
+	arc := NewArc(16)
+	if arc == nil {
+		t.Errorf("Error initializing ARC")
+	}
+
+	values := []int{1, 2, 3, 4, 5, 6, 7, 8}
+
+	for i := 0; i < len(values); i++ {
+		val := strconv.Itoa(i)
+		arc.Set(val, []byte(val))
 	}
 }
