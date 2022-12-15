a8c0005cc95a77d838ee0e854714b55f6625a7ff
Kenny <kennyl@princeton.edu>
2022-12-15 01:21:04 -0500

    added rudimentary init tests

--- arc/arc_test.go
+++ arc/arc_test.go
@@ -7,4 +7,27 @@ Description:
 
 package arc
 
-//Helper Functions:
+import (
+	"testing"
+)
+
+// Helper Functions:
+
+func TestArcInit(t *testing.T) {
+	arc := NewArc(8)
+	if arc == nil {
+		t.Errorf("Error initializing ARC")
+	}
+	if arc.RemainingStorage() != 8 {
+		t.Errorf("Size of ARC cache is %d bytes, should be 8 bytes", arc.RemainingStorage())
+	}
+	if arc.Len() != 0 {
+		t.Errorf("Bindings in ARC cache is %d, should be 0", arc.Len())
+	}
+}
+
+func TestTooSmallArcInit(t *testing.T) {
+	if NewArc(0) != nil || NewArc(1) != nil {
+		t.Errorf("initializing an ARC object even when size is incorrect")
+	}
+}
