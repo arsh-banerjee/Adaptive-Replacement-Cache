51aede4b58622e47b4d319a35a9172ae2d9a1471
arsh-banerjee <78701372+arsh-banerjee@users.noreply.github.com>
2022-12-15 19:28:15 -0500

    Adding Benchmark test to compare caches

--- arc/performance_test.go
+++ arc/performance_test.go
@@ -6,3 +6,40 @@ Description:
 *****************************************************************************/
 
 package arc
+
+import (
+	"math/rand"
+	"strconv"
+	"testing"
+)
+
+func TestCacheBenchmark(t *testing.T) {
+	capacity := 1000
+	cache := NewArc(capacity)
+
+	rand.Seed(2)
+
+	tracesize := capacity * 10
+	var trace [10000]int
+	for i := 0; i < tracesize; i++ {
+		trace[i] = rand.Intn(tracesize / 2)
+		cache.Set(strconv.Itoa(trace[i]), []byte(""))
+	}
+
+	hits := 0
+	misses := 0
+
+	for i := 0; i < tracesize; i++ {
+		trace[i] = rand.Intn(tracesize / 2)
+		_, prs := cache.Get(strconv.Itoa(trace[i]))
+		if prs {
+			hits += 1
+		} else {
+			misses += 1
+		}
+	}
+
+	ratio := float64(hits) / (float64(hits) + float64(misses))
+	t.Logf("ARC of size %d: hits: %d | misses: %d | hit-ratio: %f", capacity, hits, misses, ratio)
+
+}
